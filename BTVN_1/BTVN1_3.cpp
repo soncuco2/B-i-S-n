d48386590e536237bac639460f964bcc0c2dd0b0
soncuco2 <70886709+soncuco2@users.noreply.github.com>
2020-09-09 10:08:09 +0700

    Update BTVN1_3.cpp

--- BTVN_1/BTVN1_3.cpp
+++ BTVN_1/BTVN1_3.cpp
@@ -14,6 +14,7 @@ int main(){
 	scanf("%f", c);
 	
 	if((a + b >c) && (a + c > b) && (b + c > a)){
+		printf("Day la 3 canh cua 1 tam giac");
 		printf("Chu vi cua tam giac = ",a + b + c);
 		printf("dien tich cua tam giac = ", sqrt(p*(p-a)*(p-b)*(p-c)));
 	} else {
