fc0ed76028a2d807d24cf7e14d499fb8e02afdfc
soncuco2 <70886709+soncuco2@users.noreply.github.com>
2020-09-09 10:06:54 +0700

    Update BTVN1_3.cpp

--- BTVN_1/BTVN1_3.cpp
+++ BTVN_1/BTVN1_3.cpp
@@ -13,7 +13,7 @@ int main(){
 	printf("Nhap vao canh c = ");
 	scanf("%f", c);
 	
-	if((a + b >c) || (a + c > b) || (b + c > a)){
+	if((a + b >c) && (a + c > b) && (b + c > a)){
 		printf("Chu vi cua tam giac = ",a + b + c);
 		printf("dien tich cua tam giac = ", sqrt(p*(p-a)*(p-b)*(p-c)));
 	} else {
