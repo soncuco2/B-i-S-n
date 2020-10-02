e084e11abd42b462137c68e45e8f8a1002012571
soncuco2 <70886709+soncuco2@users.noreply.github.com>
2020-10-02 09:53:19 +0700

    Update Practical_1.cpp

--- Practical_1.cpp
+++ Practical_1.cpp
@@ -1,14 +1,14 @@
 #include<stdio.h>
-int fibonacci(int n) {
+int fibonacci(int n){
     int f0 = 0;
     int f1 = 1;
     int fn = 1;
     int i;
-    if (n < 0) {
+    if(n < 0){
         return -1;
-    }else if (n == 0 || n == 1){
+    }else if(n == 0 || n == 1){
         return n;
-    }else {
+    }else{
         for (i = 2; i < n; i++){
             f0 = f1;
             f1 = fn;
