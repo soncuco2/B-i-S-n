b906f30267d86ea4e5d04b848d2afb6100d0d964
soncuco2 <70886709+soncuco2@users.noreply.github.com>
2020-10-02 09:52:08 +0700

    Add files via upload

---
+++ Practical_1.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int fibonacci(int n) {
+    int f0 = 0;
+    int f1 = 1;
+    int fn = 1;
+    int i;
+    if (n < 0) {
+        return -1;
+    }else if (n == 0 || n == 1){
+        return n;
+    }else {
+        for (i = 2; i < n; i++){
+            f0 = f1;
+            f1 = fn;
+            fn = f0 + f1;
+        }
+    }
+    return fn;
+}
+int main() {
+ 	int i;
+    printf("day so Fibonacci nho hon 100 la : \n");
+    for (i = 0; i < 12; i++) {
+        printf("%d ", fibonacci(i));
+    }
+}
